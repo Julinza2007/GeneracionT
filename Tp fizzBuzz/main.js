4db35875bda4a0a94e669c3fa18c474cef014c23
julinza <julinza@example.com>
2025-07-15 14:49:17 -0300

    Agrego TP fizzBuzz incompleto

---
+++ Tp fizzBuzz/main.js
@@ -0,0 +1,42 @@
+function handleSubmit(e){
+    e.preventDefault();
+    let string1 = e.target.string1.value;
+    let string2 = e.target.string2.value;
+    let fizz = e.target.fizz.value;
+    let buzz = e.target.buzz.value;
+    console.log(string1, string2, fizz, buzz);
+}
+
+
+
+for(i=0; i < 100; i++){
+    if (i == )
+}
+
+
+
+/*
+Para completar este ejercicio, deberás:
+1. Lograr que fizzBuzz2 devuelva un String con los números separados
+por comas.
+2. Mejorar la Función para que el usuario pueda decidir hasta qué
+número tiene que contar fizzBuzz2.
+3. Mejorar la Función para que el usuario pueda
+ingresar fizz_num y buzz_num para que la sustitución de palabras
+ocurra en los números múltiplos de los nuevos argumentos de
+entrada (en vez de solo 3 y 5).
+
+
+Desafío FizzBuzz
+1. cuenta del 1 al 100
+    Muestra números secuenciales en la consola.
+2. Múltiplos de 3: "Fizz"
+    Sustituye cada número divisible por 3.
+
+3. Múltiplos de 5: "Buzz"
+    Sustituye cada número divisible por 5.
+
+4. Múltiplos de 3 y 5: "FizzBuzz"
+    Combina ambas palabras para múltiplos comunes.
+
+*/
